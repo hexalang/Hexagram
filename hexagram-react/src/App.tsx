be4b963d99c3b429e0d88b6c4e616f3b1943faae
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2020-04-14 19:54:41 -0700

    Use SASS

--- hexagram-react/src/App.tsx
+++ hexagram-react/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import TdClient, { TdOptions, TdObject } from 'tdweb';
 
 const options: TdOptions = {
